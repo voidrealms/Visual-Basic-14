341618042ba4200e067e4ab3d113a93c9192e587
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:39:57 -0400

    First commit

---
+++ VB 14/ConsApp list_genirics 14/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim People As List(Of String)
+
+        'list is generic.
+
+        People = New List(Of String)
+       
+        People.Add("bob") '0 
+        People.Add("marry") '1
+        People.Add("chad") '2
+
+        People.Insert(1, "heather")
+
+
+        'If People.Contains("bob") Then
+        'People.Remove("bob")
+        'End If
+
+        Dim sName As String
+        For Each sName In People
+            Console.WriteLine(sName)
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: Visual Basic 14 - Lists
+Published: 7/11/2010 12:05:57 PM
+Link: https://www.youtube.com/watch?v=fjbOAcT87rw
+
+Description: learn about lists collections and generics
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
